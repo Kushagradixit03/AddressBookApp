d46e8df98b435db00bfed9e1a5f0b9da2db842fd
Kushagradixit03 <kushagradixit0312@gmail.com>
2025-03-06 23:08:40 +0530

    [Kushagra Dixit]DTO and MODEL

--- src/main/java/com/example/AddressBookApp/Repository/AddressBookRepository.java
+++ src/main/java/com/example/AddressBookApp/Repository/AddressBookRepository.java
@@ -1,7 +1,7 @@
 package com.example.AddressBookApp.Repository;
 
 
-import com.example.AddressBookApp.Entity.AddressBook;
+import com.example.AddressBookApp.Model.AddressBook;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AddressBookRepository extends JpaRepository<AddressBook, Long> {
